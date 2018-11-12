49de663ad73d9db52423f12b1ec13ec1dc1f8c0b
Chemingui Asma <Chemingui.Asma@issatso.u-sousse.tn>
2018-11-13 00:02:15 +0100

    Ajout de methode d'affichage des articles

--- EBoutique/Controllers/HomeController.cs
+++ EBoutique/Controllers/HomeController.cs
@@ -37,5 +37,26 @@ namespace EBoutique.Controllers
         {
             return View();
         }
+        public JsonResult GetArticles()
+        {
+            iBoutiqureDBEntities dc = new iBoutiqureDBEntities();
+            List<ArticleViewModel> articles = dc.Articles.Select(x => new ArticleViewModel
+            {
+                idArticle = x.idArticle,
+                refArticle = x.refArticle,
+                libelleArticle = x.libelleArticle,
+                prix = x.prix,
+                description = x.description,
+                disponibilite = x.disponibilite,
+                nbpieces = x.nbpieces,
+                libelleMarque = x.Marque.libelleMarque,
+                libelleType = x.Type.libelleType,
+                libelleCatgorie = x.Categorie.libelleCatgorie,
+                couleur = x.couleur,
+                taille = x.taille
+            }
+            ).ToList();
+            return Json(new { data = articles }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
