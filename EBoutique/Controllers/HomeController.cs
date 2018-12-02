f983df7c9c5cb3de282e65dad68912f16154a5e7
wiem <benothmeenwiem@gmail.com>
2018-12-02 19:01:28 +0100

    liste users

--- EBoutique/Controllers/HomeController.cs
+++ EBoutique/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace EBoutique.Controllers
             return View();
 
         }
-        public ActionResult User()
+        public ActionResult ListeUsers()
         {
             return View();
         }
@@ -80,5 +80,17 @@ namespace EBoutique.Controllers
             }
             return ls;
         }
+
+        public JsonResult GetUsers()
+        {
+            using (iBoutiqureDBEntities dc = new iBoutiqureDBEntities())
+            {
+                var users = dc.Users.OrderBy(a => a.nom).ToList();
+                return Json(new { data = users }, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+
     }
 }
