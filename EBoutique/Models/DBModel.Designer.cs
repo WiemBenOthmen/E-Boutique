e0f98b3ceabe1f4cefd8b7f433bad83352ee6d51
ghoui_000 <ghoui_000@HP>
2018-12-11 02:24:15 +0100

    ajout panier

--- EBoutique/Models/DBModel.Designer.cs
+++ EBoutique/Models/DBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\ASUS\Source\Repos\WiemBenOthmen\e-boutique\EBoutique\Models\DBModel.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'D:\PROJET DOTNET\EBoutique\Models\DBModel.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
